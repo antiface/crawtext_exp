d942c0e5020971db67018488d88dc79730f766f0
Antoine Mazieres <antoine.mazieres@gmail.com>
2012-02-28 11:34:32 +0100

    printing clean absolute path of all html node

--- standalone_scripts/xpath_ranking.py
+++ standalone_scripts/xpath_ranking.py
@@ -23,19 +23,27 @@
 
 # http://stackoverflow.com/questions/8475377/python-xml-absolute-path
 
-
+from lxml.html import soupparser
 from lxml import etree
+import urllib2
 
+# if XHTML then xml parser, elif html then html parser
 
-root = etree.HTML('''
-<html><head><title>Vous Etes Perdu ?</title></head><body><h1>Perdu sur l'Internet ?</h1><h2>Pas de panique, on va vous aider</h2><strong><pre>    * <----- vous &ecirc;tes ici</pre></strong></body></html>
-''')
+# root = etree.HTML('''
+# <html><head><title>Vous Etes Perdu ?</title></head><body><h1>Perdu sur l'Internet ?</h1><h2>Pas de panique, on va vous aider</h2><strong><pre>    * <----- vous &ecirc;tes ici</pre></strong></body></html>
+# ''')
+
+# url = "http://www.perdu.com/"
+url = 'http://www.lacuisinehelene.com/2012/02/pita-with-hummus-veggies-and-feta.html'
+# doc = etree.parse()
+root = etree.HTML(urllib2.urlopen(url).read())
 
 def print_path_of_elems(elem, elem_path=""):
     for child in elem:
         if not child.getchildren():
             # leaf node with text => print
-            print "%s/%s, %s" % (elem_path, child.tag, child.text)
+#            print "%s/%s, %s" % (elem_path, child.tag, child.text)
+            print "%s/%s" % (elem_path, child.tag)
         else:
             # node with child elements => recurse
             print_path_of_elems(child, "%s/%s" % (elem_path, child.tag))
