3e6934c5031c19271bf54c752bddcaed77f23928
Antoine Mazieres <antoine.mazieres@gmail.com>
2012-03-06 09:02:15 +0100

    average text length per xpath ranking working

--- standalone_scripts/text_lengh_ranking.py
+++ standalone_scripts/text_lengh_ranking.py
@@ -16,22 +16,23 @@ import operator
 import re
 
 #url = "http://www.perdu.com/"
-url = 'http://www.lacuisinehelene.com/2012/02/pita-with-hummus-veggies-and-feta.html'
-root = etree.HTML(urllib.urlopen(url).read())
-# root = etree.HTML('''
-# <html>
-# <head>
-# <title>Vous Etes Perdu ?</title>
-# </head>
-# <body>
-# <h3></h3>
-# <h1>Perdu sur l'Internet ?</h1>
-# <h1>Perdu dans la vie ?</h1>
-# <h2>Pas de panique, on va vous aider</h2>
-# <strong><pre>    * <----- vous &ecirc;tes ici</pre></strong></body></html>
-# ''')
+# url = 'http://www.lacuisinehelene.com/2012/02/pita-with-hummus-veggies-and-feta.html'
+# root = etree.HTML(urllib.urlopen(url).read())
+root = etree.HTML('''
+<html>
+<head>
+<title>Vous Etes Perdu ?</title>
+</head>
+<body>
+<h3></h3>
+<h1>Perdu sur l'Internet ?</h1>
+<h1>Perdu dans la vie ?</h1>
+<h2>Pas de panique, on va vous aider</h2>
+<strong><pre>    * <----- vous &ecirc;tes ici</pre></strong></body></html>
+''')
 d = {}
 
+# dict path,length
 def print_path_of_elems(elem, elem_path=""):
     for child in elem:
         if not child.getchildren() and child.text:
@@ -47,13 +48,16 @@ print_path_of_elems(root, root.tag)
 sorted_d = sorted(d.iteritems(), key=operator.itemgetter(1), reverse=True)
 # print sorted_d
 
-
+# dict path,average_lenght
 for each in sorted_d:
 	last_tag = each[0].split('/')[-1]
 	match = re.search('\<.*\>',last_tag)
 	if not match and last_tag not in ['style','script']:
-		print each[0]
+		print "***xpath: %s" % each[0]
 		total = 0
 		for num in each[1]:
 			total += num
-		print (total/len(each[1]))
+		print "***average_length: %d" % (total/len(each[1]))
+		for path in root.xpath(each[0]):
+			i = 0
+			print "\n%s\n" % path.text
