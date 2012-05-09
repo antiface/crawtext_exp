be1bfbeacb9c551f46f88ce5d4886ab5bb359058
Antoine Mazieres <antoine.mazieres@gmail.com>
2012-05-09 10:58:47 +0200

    nope

--- standalone_scripts/text_lengh_ranking.py
+++ standalone_scripts/text_lengh_ranking.py
@@ -17,22 +17,23 @@ import re
 
 #url = "http://www.perdu.com/"
 #url = 'http://www.lacuisinehelene.com/2012/02/pita-with-hummus-veggies-and-feta.html'
-#url ='http://www.lexpress.fr/actualite/sciences/sante/la-bonne-algue-verte_489903.html'
-#root = etree.HTML(urllib.urlopen(url).read())
+url ='http://www.lexpress.fr/actualite/sciences/sante/la-bonne-algue-verte_489903.html'
+url ='http://www.capital-terrain.fr/#'
+root = etree.HTML(urllib.urlopen(url).read())
 
 ### SAMPLE HTML 4 DEBUG
-root = etree.HTML('''
-<html>
-<head>
-<title>Vous Etes Perdu ?</title>
-</head>
-<body>
-<h3></h3>
-<h1>Perdu sur l'Internet ?</h1>
-<h1>Perdu dans la vie ?</h1>
-<h2>Pas de panique, on va vous aider</h2>
-<strong><pre>    * <----- vous &ecirc;tes ici</pre></strong></body></html>
-''')
+# root = etree.HTML('''
+# <html>
+# <head>
+# <title>Vous Etes Perdu ?</title>
+# </head>
+# <body>
+# <h3></h3>
+# <h1>Perdu sur l'Internet ?</h1>
+# <h1>Perdu dans la vie ?</h1>
+# <h2>Pas de panique, on va vous aider</h2>
+# <strong><pre>    * <----- vous &ecirc;tes ici</pre></strong></body></html>
+# ''')
 d = {}
 
 def print_path_of_elems(elem, elem_path=""):
@@ -54,15 +55,15 @@ print max_len
 
 for each in sorted_d:
 	#This IF is a random guess: needs more example to find a better way to select top of the dict
-	if each[1][0] > (max_len / 2):
-		last_tag = each[0].split('/')[-1]
-		match = re.search('\<.*\>',last_tag)
-		if not match and last_tag not in ['style','script']:
-			print "***xpath: %s" % each[0]
-			total = 0
-			for num in each[1]:
-				total += num
-			print "***average_length: %d" % (total/len(each[1]))
-			for path in root.xpath(each[0]):
-				i = 0
-				print "\n%s\n" % path.text
+#	if each[1][0] > (max_len / 2):
+	last_tag = each[0].split('/')[-1]
+	match = re.search('\<.*\>',last_tag)
+	if not match and last_tag not in ['style','script']:
+		print "***xpath: %s" % each[0]
+		total = 0
+		for num in each[1]:
+			total += num
+		print "***average_length: %d" % (total/len(each[1]))
+		for path in root.xpath(each[0]):
+			i = 0
+			print "\n%s\n" % path.text
