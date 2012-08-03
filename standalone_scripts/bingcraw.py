6efdd7bc6b64e250c2f9f8480a2f04a03fc5edd5
Antoine Mazieres <antoine.mazieres@gmail.com>
2012-08-04 01:16:07 +0200

    boing function, args, etc

--- standalone_scripts/bingcraw.py
+++ standalone_scripts/bingcraw.py
@@ -13,54 +13,71 @@ import time
 reload(sys) 
 sys.setdefaultencoding("utf-8")
 
-username = ''
-password = 'HIDDEN'
-
-base64string = base64.encodestring('%s:%s' % (username,password) )
-
-query_words = "Algues"
-
 root_url = 'https://api.datamarket.azure.com/Bing/Search/'
-query_url = 'Web?Query=%27Algues%27&$top=10&$format=JSON&Market=%27fr-FR%27'
-full_url = root_url + query_url
+markets_list = ["ar-XA","bg-BG","cs-CZ","da-DK","de-AT","de-CH","de-DE","el-GR","en-AU","en-CA","en-GB","en-ID","en-IE","en-IN","en-MY","en-NZ","en-PH","en-SG","en-US","en-XA","en-ZA","es-AR","es-CL","es-ES","es-MX","es-US","es-XL","et-EE","fi-FI","fr-BE","fr-CA","fr-CH","fr-FR","he-IL","hr-HR","hu-HU","it-IT","ja-JP","ko-KR","lt-LT","lv-LV","nb-NO","nl-BE","nl-NL","pl-PL","pt-BR","pt-PT","ro-RO","ru-RU","sk-SK","sl-SL","sv-SE","th-TH","tr-TR","uk-UA","zh-CN","zh-HK","zh-TW"]
 
-query = urllib2.Request(full_url)
-query.add_header("Authorization", "Basic %s" % base64string )
+def query_bing(query_words, password, nb_results=10, market="fr-FR", username=''):
+	# Formatting username and password for HTTP headers 
+	base64string = base64.encodestring('%s:%s' % (username,password) )
+	
+	# Formatting query	
+	query_words = query_words.replace(' ','%20')
 
-print "Query URL: ", query.get_full_url()
-for each in query.header_items():
-	print each
+	# Checking Market validity
+	if market not in markets_list:
+		print "The market you specified is not supported by Bing API"
+		print "Markets List: " + str(markets_list)
+		sys.exit(1)
 
-try:
-	results_utf8 = unicode(urllib2.urlopen(query).read(), errors='ignore')
-except IOError as e:
-    if hasattr(e, 'reason'):
-        print 'We failed to reach a server.'
-        print 'Reason: ', e.reason
-    elif hasattr(e, 'code'):
-        print 'The server couldn\'t fulfill the request.'
-        print 'Error code: ', e.code
-        print 'Error message: ', e.msg
-        print 'Header: ', e.hdrs
-        print 'FP: ', e.fp
+	# Limiting the number of results (This limit can be overcome > TODO, check "next" in bing answer)
+	if nb_results > 50:
+		print "Results are limite to 50. Set to 50."
+		nb_results = 50
+
+	query_url = 'Web?Query=%27'+('%s' % query_words.replace(' ','%20'))+'%27&$top='+str(nb_results)+'&$format=JSON&Market=%27'+market+'%27'
+	full_url = root_url + query_url
+
+	query = urllib2.Request(full_url)
+	query.add_header("Authorization", "Basic %s" % base64string )
+
+	print "Query URL: ", query.get_full_url()
+	for each in query.header_items():
+		print each
+
+	try:
+		results_utf8 = unicode(urllib2.urlopen(query).read(), errors='ignore')
+	except IOError as e:
+	    if hasattr(e, 'reason'):
+	        print 'We failed to reach a server.'
+	        print 'Reason: ', e.reason
+	    elif hasattr(e, 'code'):
+	        print 'The server couldn\'t fulfill the request.'
+	        print 'Error code: ', e.code
+	        print 'Error message: ', e.msg
+	        print 'Header: ', e.hdrs
+	        print 'FP: ', e.fp
 
 
-connection = Connection()
-connection = Connection('localhost', 27017)
+	connection = Connection()
+	connection = Connection('localhost', 27017)
 
-timestamp = ''
-for each in time.localtime()[:]:
-	timestamp += str(each)
+	timestamp = ''
+	for each in time.localtime()[:]:
+		timestamp += str(each)
 
-db = connection['seeds_bing']
-collection_name = "%s%s" % (query_words.replace(' ',''), timestamp)
-collection = db['%s' % collection_name]
+	seen_url = set()
+	tosee_url = set()
 
-results_json = json.loads(results_utf8)
-results = results_json['d']['results']
-print type(results)
-print len(results)
-for each in results:
-	print each
-	collection.insert(each)
-	print "\n"
+	db = connection['crawbing']
+	collection_name = "%s%s" % (query_words.replace(' ',''), timestamp)
+	collection = db['%s' % collection_name]
+
+	results_json = json.loads(results_utf8)
+	results = results_json['d']['results']
+	print type(results)
+	print len(results)
+	for each in results:	
+		print each
+		collection.insert(each)
+		print "\n"
+
